db01b6f0c10aac6b00ca70cac2aa2b5636aa93b3
JPKingtero <JPQM4897@gmail.com>
2023-09-16 00:57:33 -0500

    feat(ParameterizedTests): Method to address parameterized test using @ValueSource

--- src/test/java/junit5Tests/ParameterizedTests.java
+++ src/test/java/junit5Tests/ParameterizedTests.java
@@ -1,6 +1,9 @@
 package junit5Tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParameterizedTests {
@@ -8,6 +11,14 @@ public class ParameterizedTests {
     @ParameterizedTest(name = "Iteration: {index} - value: {arguments}")
     @ValueSource(ints = {1,2,3,4,5,6,7})
     void intValues(int theParameter){
+        System.out.println("theParameter = " + theParameter);
+    }
+
+    @ParameterizedTest(name = "Iteration: {index} - value: {arguments}")
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = {"First String","Second String"})
+    void stringValues(String theParameter){
         System.out.println("theParameter = " + theParameter);
     }
 }
