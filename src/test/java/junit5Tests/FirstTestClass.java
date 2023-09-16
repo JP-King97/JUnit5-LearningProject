3c3022151d069ac640d5c63e358ead782ee0d541
JPKingtero <JPQM4897@gmail.com>
2023-09-16 00:08:16 -0500

    feat(FirstTestClass): 2 test were added as well as 4 method with tags to place them beofore and after tests and the test suite

---
+++ src/test/java/junit5Tests/FirstTestClass.java
@@ -0,0 +1,37 @@
+package junit5Tests;
+
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class FirstTestClass {
+
+    @BeforeAll
+    void setUp(){
+        System.out.println("Environment set");
+    }
+
+    @BeforeEach
+    void settingVariables(){
+        System.out.println("Variables set");
+    }
+
+    @AfterEach
+    void deletingVariablesValues(){
+        System.out.println("Variables Deleted");
+    }
+
+    @AfterAll
+    void tearDown(){
+        System.out.println("Environment shut down");
+    }
+
+    @Test
+    void firstMethod(){ // access modifier can be omitted but cannot be private
+        System.out.println("This is the first test method");
+    }
+
+    @Test
+    void secondMethod(){
+        System.out.println("This is the second test method");
+    }
+}
